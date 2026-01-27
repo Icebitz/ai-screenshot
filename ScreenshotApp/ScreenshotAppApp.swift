d7e28357a6b9ab1b93fcc9fe0e7fff2c664d04a6
chronoco-de7 <sahinmalu04@gmail.com>
2026-01-27 20:44:10 +0000

    Initial Commit

---
+++ ScreenshotApp/ScreenshotAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenshotAppApp.swift
+//  ScreenshotApp
+//
+//  Created by Admin on 1/27/26.
+//
+
+import SwiftUI
+
+@main
+struct ScreenshotAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
